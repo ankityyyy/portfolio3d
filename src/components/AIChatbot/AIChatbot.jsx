2dfa955584e15d268c353d80c3eaeffe4d952829
ankityyyy <ankitdubey9431969639@gmail.com>
2026-09-17 22:48:29 +0530

    ana

--- src/components/AIChatbot/AIChatbot.jsx
+++ src/components/AIChatbot/AIChatbot.jsx
@@ -63,7 +63,7 @@ console.log("Sending to backend:", question);
 
     try {
       const response = await fetch(
-        "http://localhost:2000/api/v1/query/063a2a39-01a8-43c2-bf85-cfce19ff35cc",
+        "https://portfol-backend.onrender.com/api/v1/query/063a2a39-01a8-43c2-bf85-cfce19ff35cc",
         { 
           method: "POST",
           headers: {
